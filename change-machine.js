a3b1d563bd61d0593447dd9b231457404643636e
brendahahaha <35875370+brendahahaha@users.noreply.github.com>
2018-07-10 16:37:38 -0700

    update

--- change-machine.js
+++ change-machine.js
@@ -1,1 +1,24 @@
-// Author: FirstName LastName
+// Author: Brenda Buth
+
+console.log("This Is The Change Machine");
+
+let readline = require("readline-sync");
+
+let cents = Number(readline.question('Please enter cents as a positive integer'));
+
+let quarters = Math.floor(cents/25);
+console.log("quarters: " + quarters);
+
+let remaindersOfQuarters = Math.floor(cents%25);
+let dimes = Math.floor(remaindersOfQuarters/10);
+console.log("dimes: " + dimes);
+
+let remaindersOfDimes = Math.floor(remaindersOfQuarters%10);
+let nickels = Math.floor(remaindersOfDimes/5);
+console.log("nickels: " + nickels);
+
+let remaindersOfNickels = Math.floor(remaindersOfDimes%5);
+let pennies = Math.floor(remaindersOfNickels/1);
+console.log("pennies: " + pennies);
+
+console.log("You're Welcome!");

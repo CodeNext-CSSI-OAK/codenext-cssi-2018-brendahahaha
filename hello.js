98a04474990420959ad590d4b54eb24c0bf02502
brendahahaha <35875370+brendahahaha@users.noreply.github.com>
2018-07-10 12:44:43 -0700

    update

--- hello.js
+++ hello.js
@@ -1,4 +1,10 @@
 // Author: Brenda Buth
 
-console.log('Hello Code Next');
-console.log('Another Line Of Code');
+let readline = require( 'readline-sync');
+
+let answer = readline.question('what is your age');
+
+let ageAsAString = readline.question('what is your age');
+let age = Number(ageAsAString);
+
+console.log('In 2028, you will be ' + (age + 10) + 'years old');
